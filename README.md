e72d0369fc2cc7f6ed880b3f3bdd02d0a3bbea0f
andersnm <andersnm@users.noreply.github.com>
2025-04-08 20:33:16 +0200

    Pages fiks and readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Modulyzer
 
-Backup
+Modular MIDI Tracker with built-in wavetable, DX7 and 303 emulation, delay and reverb effects, and more.
+
+Try it here: https://andersnm.github.io/modulyzer/
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({})
+// The application can be hosted in a subdirectory, but must be built specially for it.
+// The Github Pages build uses "vite build --mode pages" and reads VITE_BASE from .env.pages.
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_BASE || '/',
+  };
+});
